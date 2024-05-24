6913f5cc08a6327b6033d8b9b6b362691e3d644c
Mauru Maduranga <mayurumaduranga@gmail.com>
2024-05-24 15:49:38 +0530

    Tour page update

---
+++ Skyline-Frontend/src/Pages/Admin/TourManagement/TournavigationBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const TourNavigationBar = () => {
+  return (
+    <nav className="flex justify-between items-center ">
+      <div className="flex items-center">
+        <span className="text-xl font-semibold">Total Tours</span>
+        <div className="ml-2 bg-blue-500 w-2 h-6 rounded-full"></div>
+      </div>
+      <div className="flex space-x-2">
+      <Link to="/admin/allTours">
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-full  hover:bg-blue-700  font-bold  focus:outline-none focus:shadow-outline"> All Tours</button>
+        </Link>
+        <Link to="/admin/economyTours">
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-full  hover:bg-blue-700  font-bold  focus:outline-none focus:shadow-outline">Economic Tours</button>
+        </Link>
+        <Link to="/admin/businesTours">
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-full  hover:bg-blue-700  font-bold  focus:outline-none focus:shadow-outline">Business Tours</button>
+        </Link>
+      </div>
+      <Link to="/admin/addTour">
+      <button className="bg-blue-500 text-white py-2 px-4 rounded-full  hover:bg-blue-700  font-bold  focus:outline-none focus:shadow-outline">+ Add Tour</button>
+      </Link>
+    </nav>
+  );
+};
+
+export default TourNavigationBar;
